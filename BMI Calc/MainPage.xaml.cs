e6933c2944f64c81c309edd3508acc45e4039dc4
justin ramo <22100168@usc.edu.ph>
2026-02-21 23:58:07 +0800

    first commit

---
+++ BMI Calc/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿namespace BMI_Calc;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnCalculateBMI(object sender, EventArgs e)
+    {
+        if (double.TryParse(weightEntry.Text, out double weight) &&
+            double.TryParse(heightEntry.Text, out double height) &&
+            height > 0)
+        {
+            double bmi = weight / (height * height);
+            string category;
+
+            if (bmi < 18.5)
+                category = "Underweight";
+            else if (bmi < 25)
+                category = "Normal";
+            else if (bmi < 30)
+                category = "Overweight";
+            else
+                category = "Obese";
+
+            resultLabel.Text = $"BMI: {bmi:F2}\nCategory: {category}";
+        }
+        else
+        {
+            resultLabel.Text = "Please enter valid values.";
+        }
+    }
+}
---
+++ BMI Calc/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace BMI_Calc
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
